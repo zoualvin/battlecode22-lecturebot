225c6df515445aeff7b54ee78b3dcf641d5bb17e
Alvin Zou <alvinz@mit.edu>
2022-01-09 23:31:06 -0500

    made a new daddy

---
+++ src/daddydarwin/LaboratoryStrategy.java
@@ -0,0 +1,11 @@
+package daddydarwin;
+
+import battlecode.common.*;
+
+public class LaboratoryStrategy {
+    public static void runLaboratory(RobotController rc) throws GameActionException {
+        if (rc.getTeamLeadAmount(rc.getTeam()) > 7000 && rc.canTransmute()) {
+            rc.transmute();
+        }
+    }
+}
