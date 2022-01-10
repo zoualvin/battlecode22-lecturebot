36a2deac8d2a39de3e4da7565a08ab71b777bd01
Alvin Zou <alvinz@mit.edu>
2022-01-09 23:55:18 -0500

    put already written runSoldier into the SoliderStrategy class

--- src/daddydarwin/SoldierStrategy.java
+++ src/daddydarwin/SoldierStrategy.java
@@ -2,9 +2,28 @@ package daddydarwin;
 
 import battlecode.common.*;
 
+import static daddydarwin.RobotPlayer.directions;
+import static daddydarwin.RobotPlayer.rng;
+
 public class SoldierStrategy {
 
     static void runSoldier(RobotController rc) throws GameActionException {
+        // Try to attack someone
+        int radius = rc.getType().actionRadiusSquared;
+        Team opponent = rc.getTeam().opponent();
+        RobotInfo[] enemies = rc.senseNearbyRobots(radius, opponent);
+        if (enemies.length > 0) {
+            MapLocation toAttack = enemies[0].location;
+            if (rc.canAttack(toAttack)) {
+                rc.attack(toAttack);
+            }
+        }
 
+        // Also try to move randomly.
+        Direction dir = directions[rng.nextInt(directions.length)];
+        if (rc.canMove(dir)) {
+            rc.move(dir);
+            System.out.println("I moved!");
+        }
     }
 }
