93ec5f42f665952ab37927bb3b3c04784f20d8f6
Alex <ahw23475@gmail.com>
2022-01-10 00:51:47 -0500

    please work

--- src/daddydarwin/SoldierStrategy.java
+++ src/daddydarwin/SoldierStrategy.java
@@ -12,14 +12,40 @@ public class SoldierStrategy {
         int radius = rc.getType().actionRadiusSquared;
         Team opponent = rc.getTeam().opponent();
         RobotInfo[] enemies = rc.senseNearbyRobots(radius, opponent);
+        
         if (enemies.length > 0) {
-            MapLocation toAttack = enemies[0].location;
-            if (rc.canAttack(toAttack)) {
+        	MapLocation toAttack = enemies[0].location;
+        	//attack priority: enemy archons, soldiers, watchtowers, labs, then whatever's closest. take into account rubble later.
+        	for (int i = 0; i < enemies.length; i++) {
+        		if(enemies[i].getType() == battlecode.common.RobotType.ARCHON) {
+        			 toAttack = enemies[i].location;
+        			break;
+        		} else if (enemies[i].getType() == battlecode.common.RobotType.SOLDIER) {
+        			 toAttack = enemies[i].location;
+        			break;
+        			
+        		} else if (enemies[i].getType() == battlecode.common.RobotType.WATCHTOWER) {
+        			 toAttack = enemies[i].location;
+        			break;
+        			
+        		} else if (enemies[i].getType() == battlecode.common.RobotType.LABORATORY) {
+        			 toAttack = enemies[i].location;
+        			break;
+        		} else if (enemies[i].getType() == battlecode.common.RobotType.SAGE) {
+        			 toAttack = enemies[i].location;
+        			break;
+        		}
+        		
+        	}
+           
+        	if (rc.canAttack(toAttack)) {
                 rc.attack(toAttack);
             }
+        		
+        
         }
 
-        // Also try to move randomly.
+        // Also try to move randomly. // change this
         Direction dir = directions[rng.nextInt(directions.length)];
         if (rc.canMove(dir)) {
             rc.move(dir);
