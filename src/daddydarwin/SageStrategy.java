99a5a9ba9cf4bc50c6d9284ec6dd60669b101bee
Alvin Zou <alvinz@mit.edu>
2022-01-10 00:19:14 -0500

    daddy has been evolved by granddaddy darwin:

--- src/daddydarwin/SageStrategy.java
+++ src/daddydarwin/SageStrategy.java
@@ -5,6 +5,20 @@ import battlecode.common.*;
 public class SageStrategy {
 
     static void runSage(RobotController rc) throws GameActionException {
+        /**
+         * when to call an abyss:
+         */
 
+        /**
+         * when to call charge:
+         */
+
+        /**
+         * when to call fury:
+         */
+
+        /**
+         * how to read forecast of anomalies:
+         */
     }
 }
