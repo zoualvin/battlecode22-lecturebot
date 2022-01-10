b19ff5743cf26ec3cd2f558395e5ac8ce7cfad1f
Alex <ahw23475@gmail.com>
2022-01-10 15:18:13 -0500

    watchtower owo

--- src/daddydarwin/WatchTowerStrategy.java
+++ src/daddydarwin/WatchTowerStrategy.java
@@ -12,6 +12,30 @@ strictfp class WatchTowerStrategy{
         RobotInfo[] enemies = rc.senseNearbyRobots(radius, opponent);
         if (enemies.length > 0) {
             MapLocation toAttack = enemies[0].location;
+            
+            for (int i = 0; i < enemies.length; i++) {
+        		if(enemies[i].getType() == battlecode.common.RobotType.ARCHON) {
+        			 toAttack = enemies[i].location;
+        			 
+        			break;
+        		} else if (enemies[i].getType() == battlecode.common.RobotType.SOLDIER) {
+        			 toAttack = enemies[i].location;
+        			break;
+        			
+        		} else if (enemies[i].getType() == battlecode.common.RobotType.WATCHTOWER) {
+        			 toAttack = enemies[i].location;
+        			break;
+        			
+        		} else if (enemies[i].getType() == battlecode.common.RobotType.LABORATORY) {
+        			 toAttack = enemies[i].location;
+        			break;
+        		} else if (enemies[i].getType() == battlecode.common.RobotType.SAGE) {
+        			 toAttack = enemies[i].location;
+        			break;
+        		}
+        		
+        	}
+            
             if (rc.canAttack(toAttack)) {
                 rc.attack(toAttack);
             }
