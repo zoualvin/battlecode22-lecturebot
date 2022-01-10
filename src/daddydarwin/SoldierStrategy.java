651990887b95dd6a4d6938242b97cd346dc7c562
Alex <ahw23475@gmail.com>
2022-01-10 14:39:43 -0500

    improved soldier movement

--- src/daddydarwin/SoldierStrategy.java
+++ src/daddydarwin/SoldierStrategy.java
@@ -12,13 +12,18 @@ public class SoldierStrategy {
         int radius = rc.getType().actionRadiusSquared;
         Team opponent = rc.getTeam().opponent();
         RobotInfo[] enemies = rc.senseNearbyRobots(radius, opponent);
+        MapLocation target = null;
+        Direction dir = null;
         
         if (enemies.length > 0) {
         	MapLocation toAttack = enemies[0].location;
+        	dir = rc.getLocation().directionTo(enemies[rng.nextInt(enemies.length)].location);
+        	
         	//attack priority: enemy archons, soldiers, watchtowers, labs, then whatever's closest. take into account rubble later.
         	for (int i = 0; i < enemies.length; i++) {
         		if(enemies[i].getType() == battlecode.common.RobotType.ARCHON) {
         			 toAttack = enemies[i].location;
+        			 target = enemies[i].location;
         			break;
         		} else if (enemies[i].getType() == battlecode.common.RobotType.SOLDIER) {
         			 toAttack = enemies[i].location;
@@ -45,19 +50,24 @@ public class SoldierStrategy {
         
         }
 
-        // Also try to move randomly. // change this
+    
         
-        //directionTo(MapLocation location)
-        //isWithinDistanceSquared(MapLocation location, int distanceSquared)
-        Direction dir = rc.getLocation().directionTo(enemies[rng.nextInt(enemies.length)].location);
-        if (rc.canMove(dir)) {
-            rc.move(dir);
-            System.out.println("I moved!");
+        //if archon is sensed, move towards it with pathing
+        //if not, move in direction of random enemy on the map.
+        //if no enemies detected, move in a random direction
+        if(target != null) {
+        	Pathing.walkTowards(rc, target);
+        } else if (dir != null){
+        	if (rc.canMove(dir)) {
+        		rc.move(dir);
+        		System.out.println("I moved!");
+        	}
         } else {
         	dir = directions[rng.nextInt(directions.length)];
         	if (rc.canMove(dir)) {
                 rc.move(dir);
                 System.out.println("I moved!");}
         }
+        
     }
 }
